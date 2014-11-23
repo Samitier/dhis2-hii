5f847ea1edf8a18033ecb815d738992355122810
Samitier <blai.samitier@gmail.com>
2014-11-23 13:33:16 +0100

    detail page divided into directives

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -24,39 +24,13 @@ hiiControllers.controller('listController', function($scope, $location, $transla
 
 hiiControllers.controller('detailController', function($scope, $routeParams, dhis2APIService) {
 	this.init = function() {
+	    $scope.complexId = $routeParams.complexId;
 	    dhis2APIService.getTrackedEntityById($routeParams.complexId).then(function(dat){
 	        $scope.complexInfo = dat;
 	    }); 
 		$scope.showOrgUnitTree(false);
-		$scope.editing=false;
 	    this.tab =1;
-	    this.showError = false;
 	};
-    this.send = function() {
-        /*var message = {trackedEntity: $scope.complexData.tableContents[$scope.indexComplex][4], 
-                       orgUnit: $scope.complexData.tableContents[$scope.indexComplex][3], 
-                       attributes: []};
-        for(var i=5; i< this.inputInfo.length; ++i) {
-            message.attributes.push({attribute:$scope.complexData.tableHeaders[i].name, value: this.inputInfo[i]});
-        }
-        console.dir(message);*/
-        dhis2APIService.updateComplexInfo($routeParams.complexId,$scope.editForm).then(function(dat){
-            if(dat) {
-            	$scope.complexInfo = $scope.editForm;
-            	$scope.editing = false;
-            }
-            else this.showError= true; //currently not showing error
-        });
-    };
-    this.isEditing = function() {
-        return $scope.editing;
-    };
-    this.setEditing = function(edit){
-        $scope.editing = edit;
-        showError = false;
-        $scope.editForm={};
-        if($scope.editing) angular.copy($scope.complexInfo, $scope.editForm);
-    };
     this.isTab = function(tab) {
         return this.tab ==tab;
     };
--- scripts/directives.js
+++ scripts/directives.js
@@ -28,91 +28,61 @@
             controllerAs:'leftBarCtrl'
         };
     });
-/*
-    hiiDirectives.directive('mainPage', function() {
-        return {
-            restrict : 'E',
-            templateUrl: 'templates/main-page.html',
-            controller: function($scope,$translate, dhis2APIService) {
-                dhis2APIService.getProgramId('Sanitary Complex Basic Info').then(function(data) {
-                    $scope.complexProgramID = data;
-                    dhis2APIService.getTrackedEntitiesByProgram($scope.complexProgramID).then(function(dat){
-                        $scope.complexData = dat;
-                    })
-                });
-                dhis2APIService.getUserUiLocale().then(function(dat){
-                    var locale = dat;
-                    $translate.uses(locale);
-                });
-                this.setBasicInfoPanel = function(index) {
-                    $scope.indexComplex = index;
-                    $scope.complexDataForm = [];
-                    //$scope.selectedComplexInfo = {id:val[0], orgunit:val[3], trackedEntity:val[4]}; //la tracked entiy hauria de ser un valor global al inicialitzar
-                    /*for(var i=5; i<$scope.complexData.tableHeaders.length;++i) {
-                        $scope.complexDataForm.push({id:$scope.complexData.tableHeaders[i], 
-                            value: $scope.complexData.tableContents[$scope.indexComplex][i]});
-                    }
-                    console.dir($scope.complexDataForm);*/
-                   /* $scope.setTab(1);
-                    $scope.setPage(2);
-                };
-                
-            },
-            controllerAs: 'tableCtrl'
-        };
 
-    });
-
-    hiiDirectives.directive('settingsPage', function() {
+    hiiDirectives.directive('basicInfoTab', function() {
         return {
-            restrict : 'E',
-            templateUrl: 'templates/settings.html',
-            controller: function() {
-            },
-            controllerAs: 'settingsCtrl'
-        };
-    });
-
-    hiiDirectives.directive('detailPage', function() {
-        return {
-            restrict : 'E',
-            templateUrl: 'templates/detail-page.html',
+            restrict: 'E',
+            templateUrl: 'views/basic-info-tab.html',
             controller: function($scope, dhis2APIService) {
-                $scope.tab =1;
-                this.editing=false;
+                
+                $scope.editForm = {};
+                $scope.editing=false;
                 this.showError = false;
-                this.inputInfo =[];
+                
                 this.send = function() {
-                    var message = {trackedEntity: $scope.complexData.tableContents[$scope.indexComplex][4], 
-                                   orgUnit: $scope.complexData.tableContents[$scope.indexComplex][3], 
-                                   attributes: []};
-                    for(var i=5; i< this.inputInfo.length; ++i) {
-                        message.attributes.push({attribute:$scope.complexData.tableHeaders[i].name, value: this.inputInfo[i]});
-                    }
-                    console.dir(message);
-                    dhis2APIService.updateComplexInfo($scope.complexData.tableContents[$scope.indexComplex][0],message).then(function(dat){
-                        console.dir(dat);
+                    dhis2APIService.updateComplexInfo($scope.complexId,$scope.editForm).then(function(dat){
+                        if(dat) {
+                            $scope.complexInfo = $scope.editForm;
+                            $scope.editing = false;
+                        }
+                        else this.showError= true; //currently not showing error
                     });
                 };
+
                 this.isEditing = function() {
-                    return this.editing;
+                    return $scope.editing;
                 };
+
                 this.setEditing = function(edit){
-                    this.editing = edit;
+                    $scope.editing = edit;
                     showError = false;
-                    if(this.editing) for(var i=5; i<11;++i) this.inputInfo[i] = $scope.complexData.tableContents[$scope.indexComplex][i];
-                };
-                $scope.isTab = function(tab) {
-                    return $scope.tab ==tab;
-                };
-                $scope.setTab = function(tab) {
-                    $scope.tab = tab;
+                    $scope.editForm={};
+                    if($scope.editing) angular.copy($scope.complexInfo, $scope.editForm);
                 };
             },
-            controllerAs: 'detailCtrl'
+            controllerAs:'basicInfoCtrl'
         };
     });
-*/
+
+    hiiDirectives.directive('reportTab', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/report-tab.html',
+            controller: function($scope) {
+            },
+            controllerAs:'reportCtrl'
+        };
+    });
+
+    hiiDirectives.directive('buildingsTab', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/buildings-tab.html',
+            controller: function($scope) {
+            },
+            controllerAs:'buildingsTab'
+        };
+    });
 
 })();
 
